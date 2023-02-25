769e39804eed19e9954d5c4dbf19c3d54d9b3b78
Anuj kumar <anuj26122000@gmail.com>
2023-02-25 19:17:48 +0530

    eror solve in chatrow page

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -28,7 +28,7 @@ const ChatRow = ({id} : Props) => {
 
     const removeChat = async () => {
         await deleteDoc(doc(db, "users", session?.user?.email!, "chats", id))
-        router.
+        
     }
     
 
